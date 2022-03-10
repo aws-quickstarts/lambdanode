5ed3435d13cbcff9fccbf6f3439275e551740d9d
Gianluca <gian.hd@gmail.com>
2022-03-10 08:59:37 -0300

    ajustes

--- lambda/index.js
+++ lambda/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify("Hello, NodeJS!"),
+        body: JSON.stringify(process.env.message),
     };
 
     return response;
